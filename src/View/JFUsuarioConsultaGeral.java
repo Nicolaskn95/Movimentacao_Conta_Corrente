be25f236194e20918fbb834e360f72ed3eafd30b
Nicolaskn95 <nicolasjap@hotmail.com>
2025-08-30 00:23:20 -0300

    feat: implementaion export to PDF

--- src/View/JFUsuarioConsultaGeral.java
+++ src/View/JFUsuarioConsultaGeral.java
@@ -48,7 +48,7 @@ public class JFUsuarioConsultaGeral extends javax.swing.JFrame {
         jTable1 = new javax.swing.JTable();
         btnPrint = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
